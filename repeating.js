ac59750ea78fcb70dd30bdf1e152765791eda46c
RyanChehab <ryanchehab20@gmail.com>
2025-02-03 18:48:39 +0200

    hashmap

--- repeating.js
+++ repeating.js
@@ -16,3 +16,23 @@ function rep(str){
 }
 
 console.log(rep('ayyywooo'))
+
+function repHash(str){
+
+    let count = new Map()
+
+    for (let char of str){
+        count.set(char, (count.get(char)|| 0) + 1)
+    }    
+    
+    for (let char of str){
+        if (count.get(char)===1){
+            return char
+        }
+    }
+
+    return null
+
+}
+
+console.log(repHash('aayyyooo'))
