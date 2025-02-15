49cceda6e2e03bb95f40f3e41a1adbd25a76ee86
RyanChehab <ryanchehab20@gmail.com>
2025-02-15 04:31:41 +0200

    unique char

---
+++ uniqueChar.js
@@ -0,0 +1,17 @@
+function unique(str){
+    charCount = {}
+
+    for (let char of str){
+        charCount[char] = (charCount[char]||0) + 1 
+    }
+
+    for (let char of str){
+        if(charCount[char] == 1){
+            return char 
+        }
+    }
+
+    return null
+}
+
+console.log(unique("nana"))
